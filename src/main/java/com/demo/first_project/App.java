b010bd9a98e41446eadc90524740e3a5e4e49da1
nitesh <niteshballa.devops@gmail.com>
2024-01-16 20:55:50 +0530

    first commit

---
+++ src/main/java/com/demo/first_project/App.java
@@ -0,0 +1,13 @@
+package com.demo.first_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
